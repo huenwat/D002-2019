5a9145fbb0dccb0651fa01057c3dc4c632f84873
Timothy-IP <53845637+Timothy-IP@users.noreply.github.com>
2019-08-07 12:31:34 +0800

    Add files via upload

---
+++ L1/q1.py
@@ -0,0 +1,6 @@
+exercise = 1
+print(8*3.57)
+print(5+30*20)
+print((5+30)*20)
+print(1+(2+20*3)/(4*2))
+print(1+2**10)
---
+++ L1/q2.py
@@ -0,0 +1,5 @@
+exercise = 2
+from math import *
+r = sqrt(100/pi)
+A = (2*r + 6)**2
+print(r,A)
