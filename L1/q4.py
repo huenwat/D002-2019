bda0e428ce81c4ced762154f9297178124c7481b
Timothy-IP <53845637+Timothy-IP@users.noreply.github.com>
2019-08-09 09:57:27 +0800

    Update q4.py

--- L1/q4.py
+++ L1/q4.py
@@ -1,11 +1,11 @@
 from math import *
 y = input("which year?\n")
-x = int(y) % 100
 if int(y) % 400 ==0:
     print("it's a leap year")
-elif x == 0:
+elif int(y) % 100 == 0:
     print("it's not a leap year")
 elif int(y) % 4 == 0 :
     print("it's a leap year") 
 else :
     print("it's not a leap year")
+ 
